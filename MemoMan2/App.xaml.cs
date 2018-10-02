279a6c7c71854c9cef43b66851ef37a8abd67d2c
AntiqueR <antique.pendulum@gmail.com>
2018-10-03 00:20:30 +0900

    complate animation function
    #ファイルとの兼ね合い
    #ファイル数による生成の改善

--- MemoMan2/App.xaml.cs
+++ MemoMan2/App.xaml.cs
@@ -5,6 +5,9 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
 
 namespace MemoMan2
 {
@@ -12,10 +15,43 @@ namespace MemoMan2
     /// App.xaml の相互作用ロジック
     /// </summary>
     public partial class App : Application
-    {   
-    }
-    public class WindowViewModel
     {
-        public static double OpacityLimit { get; set; } = 0.2;
+        public static string PATH = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\ProjectATR\MemoMan2";
+        public static readonly string Filepath = PATH + @"\memo";
+        public static readonly string _Filepath = PATH + @"\.memo";
+
+        public static string FileRead(int number)
+        {
+            string text = "";
+            if (File.Exists(App.Filepath + number))
+            {
+                using (var stream = new StreamReader(App.Filepath + number, Encoding.UTF8))
+                {
+                    text = stream.ReadToEnd();
+                }
+            }
+            return text;
+        }
+
+        public static void FileWrite(int number, string text)
+        {
+            try
+            {
+                using (var stream = new StreamWriter(App.Filepath + number, false, Encoding.UTF8))
+                {
+                    stream.Write(text);
+                }
+            }
+            catch { }
+        }
+        public static void PointWrite(double left, double top, int number)
+        {
+            using (StreamWriter sw = new StreamWriter(App._Filepath + number, false, Encoding.GetEncoding("utf-8")))
+            {
+                sw.WriteLine("DO_NOT_DELETE");
+                sw.WriteLine("Left:" + left);
+                sw.WriteLine("Top:" + top);
+            }
+        }
     }
 }
--- MemoMan2/MainWindow.xaml.cs
+++ MemoMan2/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Diagnostics;
+using System.IO;
 
 namespace MemoMan2
 {
@@ -20,26 +22,103 @@ namespace MemoMan2
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        private int Number; 
+
+        public MainWindow() : this(0)
+        {
+
+        }
+
+        public MainWindow(int number)
         {
             InitializeComponent();
-            
+            this.Number = number;
+
+            //初期起動状態
+            if (number == 0)
+            {
+                
+                //フォルダがあるかどうか--新規作成
+                if (!Directory.Exists(App.PATH)) Directory.CreateDirectory(App.PATH);
+                else
+                {
+                    int filecount;
+                    if( (filecount = Directory.GetFiles(App.PATH,"memo*",SearchOption.TopDirectoryOnly).Length) > 1){
+                        for (int i = 1; i < filecount; i++)
+                        {
+                            if (File.Exists(App.Filepath + i))
+                            {
+                                var newmemo = new MainWindow(i);
+                                newmemo.Show();
+                            }
+                        }
+                    }
+                }
+            }
+
+            //ファイルが見つかるか確認
+            if(File.Exists(App.Filepath + this.Number))
+            {
+                this.MoonLight.Text = App.FileRead(this.Number);
+            }
+            else
+            {
+                File.CreateText(App.Filepath + this.Number);
+            }
+            this.MoonLight.TextChanged += MoonLight_TextChanged;
+
+            if (!PointReader())
+            {
+                App.PointWrite(this.Left, this.Top, this.Number);
+            }
+
         }
 
         private void CloseButton_Click(object sender, RoutedEventArgs e)
         {
+            if(File.Exists(App.Filepath + this.Number)) File.Delete(App.Filepath + this.Number);
+            if (File.Exists(App._Filepath + this.Number)) File.Delete(App._Filepath + this.Number);
             this.Close();
         }
 
         private void NewButton_Click(object sender, RoutedEventArgs e)
         {
-            var newmemo = new MainWindow();
+            int num = 1;
+            int filecount = Directory.GetFiles(App.PATH, "*", SearchOption.TopDirectoryOnly).Length;
+            if (filecount > 0) num = filecount;
+            var newmemo = new MainWindow(filecount);
             newmemo.Show();
         }
 
         private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             this.DragMove();
+            App.PointWrite(this.Left, this.Top, this.Number);
+        }
+
+        private void MoonLight_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            App.FileWrite(this.Number, this.MoonLight.Text);
+        }
+
+        private bool PointReader()
+        {
+            if (File.Exists(App._Filepath + this.Number))
+            {
+                using (StreamReader sr = new StreamReader(App._Filepath + this.Number, Encoding.GetEncoding("utf-8")))
+                {
+                    string data;
+                    while ((data = sr.ReadLine()) != null)
+                    {
+                        if (data.IndexOf("Left:") != -1) this.Left = double.Parse(data.Replace("Left:", ""));
+                        if (data.IndexOf("Top:") != -1) this.Top = double.Parse(data.Replace("Top:", ""));
+                    }
+                }
+            }
+            else return false;
+
+            return true;
         }
     }
+
 }

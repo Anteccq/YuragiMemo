1a1719bee79018879fd6f0d670fcf016e552a0f7
AntiqueR <antique.pendulum@gmail.com>
2018-10-07 03:14:35 +0900

    Jsonへのセーブ化完了

--- MemoMan2/MainWindow.xaml.cs
+++ MemoMan2/MainWindow.xaml.cs
@@ -23,27 +23,75 @@ namespace MemoMan2
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        //データ用リスト
         private static List<SaveData> datas = new List<SaveData>();
 
-        private SaveData Data;
+        private SaveData _datas;
+        private double rightEnd;
 
-        //最初に呼び出される
-        public MainWindow() : this(new SaveData())
+        private SaveData Data
         {
-            
+            get
+            {
+                return _datas;
+            }
+            set
+            {
+                this._datas = value;
+                this.Left = _datas.Left;
+                this.Top = _datas.Top;
+                this.MoonLight.Text = _datas.Text;
+            }
+        }
+
+        //初期状態での呼び出し
+        public MainWindow()
+        {
+
+            if (!Directory.Exists(App.PATH)) Directory.CreateDirectory(App.PATH);
+            InitializeComponent();
+            rightEnd = SystemParameters.WorkArea.Width - this.Width;
+
+            //ファイルが存在
+            if (File.Exists(App.Filepath))
+            {
+                var rawdatas = JsonConvert.DeserializeObject<List<SaveData>>(File.ReadAllText(App.Filepath));
+                var isFirst = true;
+                foreach(var data in rawdatas)
+                {
+                    if (isFirst)
+                    {
+                        Data = data;
+                        datas.Add(Data);
+                        isFirst = false;
+                        continue;
+                    }
+                    var newmemo = new MainWindow(data);
+                    newmemo.Show();
+                }
+            }
+            else
+            {
+                Data = new SaveData();
+                datas.Add(Data);
+            }
+            this.MoonLight.TextChanged += MoonLight_TextChanged;
+
         }
         public MainWindow(SaveData data)
         {
             InitializeComponent();
             Data = data;
             datas.Add(Data);
-
+            Debug.WriteLine("ばちっ");
             this.MoonLight.TextChanged += MoonLight_TextChanged;
         }
 
         private void CloseButton_Click(object sender, RoutedEventArgs e)
         {
             datas.Remove(Data);
+            DataWrite();
             this.Close();
         }
 
@@ -56,11 +104,27 @@ namespace MemoMan2
         private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             this.DragMove();
+            if (this.Left >= rightEnd) this.Left = rightEnd;
+            if (this.Left < 0) this.Left = 0;
+            Data.Left = this.Left;
+            Data.Top = this.Top;
+            DataWrite();
         }
 
         private void MoonLight_TextChanged(object sender, TextChangedEventArgs e)
         {
+            Data.Text = this.MoonLight.Text;
+            DataWrite();
+        }
 
+        private static void DataWrite()
+        {
+            var json = JsonConvert.SerializeObject(datas, Formatting.Indented);
+            if (!Directory.Exists(App.PATH)) Directory.CreateDirectory(App.PATH);
+            using(var stream = new StreamWriter(App.Filepath, false, Encoding.UTF8))
+            {
+                stream.Write(json);
+            }
         }
     }
 
